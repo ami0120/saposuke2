dc4d2d221f05529fe8fa0dee6942d6d8cdcb951c
ami0120 <a.fujii0120@gmail.com>
2021-07-13 23:02:57 +0900

    JSON変換

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,6 +13,7 @@ require("jquery")
 import "../stylesheets/application"
 import "../js/hoge.js"
 
+window.$ = jQuery;
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
