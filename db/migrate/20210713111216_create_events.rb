1bb459f29aaa858929d3e40bed473132182e57a3
ami0120 <a.fujii0120@gmail.com>
2021-07-13 20:17:11 +0900

    eventモデルの作成

--- db/migrate/20210713111216_create_events.rb
+++ db/migrate/20210713111216_create_events.rb
@@ -1,6 +1,9 @@
 class CreateEvents < ActiveRecord::Migration[6.0]
   def change
     create_table :events do |t|
+      t.string     :title,    nill: false
+      t.datetime   :start,    nill: false
+      t.datetime   :end,      nill: false
 
       t.timestamps
     end
